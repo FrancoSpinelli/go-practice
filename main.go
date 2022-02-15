14a26eba98fa434d53cc31782560f139f9e6cb43
FrancoSpinelli <franco_spinelli@hotmail.com>
2022-02-15 13:41:13 -0300

    person pkg

--- main.go
+++ main.go
@@ -2,32 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/FrancoSpinelli/go-practice/person"
 )
 
-type Person struct {
-	Id   uint
-	Name string
-	Age  uint8
-	Live bool
-}
-
-func (p *Person) greet() {
-	fmt.Printf("Welcome back %s\n", p.Name)
-}
-
-func (p *Person) changeName(name string) {
-	p.Name = name
-}
-
 func main() {
-	Franco := Person{
-		5,
-		"Franco Spinelli",
-		23,
-		true,
-	}
-	Franco.greet()
-	Franco.changeName("Max Power")
-	Franco.greet()
+
+	franco := person.New("Franco", "Spinelli", 23, true)
+	fmt.Printf("%+v \n", franco)
 
 }
--- person/person.go
+++ person/person.go
@@ -1,5 +1,9 @@
 package person
 
+import (
+	"fmt"
+)
+
 type person struct {
 	firstName string
 	lastName  string
@@ -7,6 +11,7 @@ type person struct {
 	adult     bool
 }
 
+// New constructor to person
 func New(firstName, lastname string, age uint8, adult bool) *person {
 	if age == 0 {
 		age = 99
@@ -17,4 +22,21 @@ func New(firstName, lastname string, age uint8, adult bool) *person {
 		age:       age,
 		adult:     adult,
 	}
+}
+
+func (p *person) SetFirstName(firstName string) { p.firstName = firstName }
+func (p *person) FirstName() string             { return p.firstName }
+
+func (p *person) SetLastName(lastName string) { p.lastName = lastName }
+func (p *person) LastName() string            { return p.lastName }
+
+func (p *person) SetAge(age uint8) { p.age = age }
+func (p *person) Age() uint8       { return p.age }
+
+func (p *person) SetAdult(adult bool) { p.adult = adult }
+func (p *person) Adult() string       { return p.firstName }
+
+// Greet to person
+func (p *person) Greet() {
+	fmt.Printf("Welcome back %s %s", p.firstName, p.lastName)
 }
