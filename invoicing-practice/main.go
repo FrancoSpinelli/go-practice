b311f01ca587373cf9ee810ba31dcad551668f96
FrancoSpinelli <franco_spinelli@hotmail.com>
2022-02-20 19:15:20 -0300

    invoicing-practice part 2 (types)

--- invoicing-practice/main.go
+++ invoicing-practice/main.go
@@ -13,11 +13,11 @@ func main() {
 		"Argentina",
 		"Capital",
 		customer.New("Franco", "Av. Cordoba", "1555896981"),
-		[]invoiceitem.Item{
+		invoiceitem.NewItem(
 			invoiceitem.New(1, "Yerba", 5.89),
 			invoiceitem.New(2, "Mate", 7.98),
 			invoiceitem.New(3, "Bombilla", 1.66),
-		},
+		),
 	)
 	item1.SetTotal()
 	fmt.Printf("%+v", item1)
--- invoicing-practice/pkg/invoiceitem/invoiceitem.go
+++ invoicing-practice/pkg/invoiceitem/invoiceitem.go
@@ -12,7 +12,25 @@ func New(id uint, product string, value float64) Item {
 	return Item{id, product, value}
 }
 
-// Value getter of Item.Value
+/* // Value getter of Item.Value
 func (i Item) Value() float64 {
 	return i.value
+} */
+
+type Items []Item
+
+func NewItem(items ...Item) Items {
+	var is Items
+	for _, item := range items {
+		is = append(is, item)
+	}
+	return is
+}
+
+func (is Items) Total() float64 {
+	var total float64
+	for _, item := range is {
+		total += item.value
+	}
+	return total
 }
